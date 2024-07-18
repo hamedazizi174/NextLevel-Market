64b9a843dd9f4588ffe66e34472ed169b54c336c
hamed <hamedazizi1374@gmail.com>
2024-07-18 09:53:31 +0330

    feat: create admin page

---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,10 @@
+import AdminLayout from "@/layouts/AdminLayout/AdminLayout";
+import AdminTemplate from "@/templates/Admin/Admin";
+
+export default function AdminPage() {
+  return (
+    <AdminLayout>
+      <AdminTemplate />
+    </AdminLayout>
+  );
+}
