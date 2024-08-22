2fafa163ba840736d6069d8c762ee989c93882cb
hamed <hamedazizi1374@gmail.com>
2024-08-22 19:37:51 +0330

    fix: change of categories cards

--- src/components/home/Categories/Categories.tsx
+++ src/components/home/Categories/Categories.tsx
@@ -1,63 +1,58 @@
 import { useGetCategories } from "@/api/categories/categories.queries";
 import { pageLocalization } from "@/constant/localization";
 import { CategoryType } from "@/types/types";
-import { Box, Stack, Typography } from "@mui/material";
-import Image from "next/image";
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Stack,
+  Typography,
+} from "@mui/material";
 import Link from "next/link";
 
 export default function Categories() {
-  const { data: categories, isLoading, isError } = useGetCategories();
+  const { data: categories } = useGetCategories();
   return (
-    <Box component="section" mb={5} p={4}>
-      {isLoading && <Typography variant="h6">Loading ...</Typography>}
-      {isError && <Typography variant="h6">Error</Typography>}
-      <Stack gap={3} px={3}>
+    <Box mb={5} p={4}>
+      <Stack gap={3}>
         <Stack
           direction="row"
           justifyContent="space-between"
           alignItems="center"
         >
-          <Typography variant="h3" fontSize="1.5rem" fontWeight="bold">
+          <Typography variant="h5" fontWeight="bold">
             {pageLocalization.home.shopOurTopCategories}
           </Typography>
-          <Link href="#">
-            <Typography variant="h3" fontSize="1.2rem" fontWeight="500">
-              {pageLocalization.home.allCategories}
-            </Typography>
-          </Link>
+          <Typography variant="h5" fontWeight="bold">
+            {pageLocalization.home.allCategories}
+          </Typography>
         </Stack>
         <Stack direction="row" gap="70px" pb="8px">
           {categories?.data.categories.map(
             (category: CategoryType, index: number) => {
               return (
-                <Link
-                  href={`/categories/${category?.name}`}
-                  style={{ position: "relative" }}
-                  key={category._id}
-                >
-                  <Typography
-                    variant="h5"
-                    position="absolute"
-                    top="0.75rem"
-                    left="50%"
-                    color="white"
-                    fontSize="1.25rem"
-                    fontWeight="600"
-                    sx={{
-                      transform: "translateX(-50%)",
-                    }}
-                  >
-                    {pageLocalization.admin.categoriesArray[index]}
-                    {/* {category?.name} */}
-                  </Typography>
-                  <Image
-                    src={`http://localhost:8000/images/categories/icons/${category?.icon}`}
-                    alt={category?.name}
-                    height={296}
-                    width={230}
-                    style={{ objectFit: "contain" }}
-                  />
-                </Link>
+                <Card sx={{ borderRadius: 3 }} key={category._id}>
+                  <CardActionArea href={`/categories/${category?.name}`}>
+                    <CardMedia
+                      image={`http://localhost:8000/images/categories/icons/${category?.icon}`}
+                      sx={{
+                        height: "256px",
+                        width: "230px",
+                        transition: "transform 0.5s ease",
+                        "&:hover": {
+                          transform: "scale(1.1)",
+                        },
+                      }}
+                    />
+                    <CardContent sx={{ textAlign: "center" }}>
+                      <Typography variant="h5" fontWeight="600">
+                        {pageLocalization.admin.categoriesArray[index]}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
               );
             }
           )}
